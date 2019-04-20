97ea18f6a5ab0e507ed44cb7dc57eda16d0af518
Jeffrey Gallegos <jeffreygallegos01@gmail.com>
2019-04-20 14:08:36 -0700

    Added comments to each of my private attributes.

--- php/classes/Author.php
+++ php/classes/Author.php
@@ -6,20 +6,41 @@ require_once (dirname(__DIR__, 2) . "/composer.json/autoload.php");
 
 use Ramsey\Uuid\Uuid;
 /**
- * Cross Section of a Twitter Profile
- *
- * This is a cross section of what is probably stored about a Twitter user. This entity is a top level entity that
- * holds the keys to the other entities in this example (i.e., Favorite and Profile).
- *
- * @author Dylan McDonald <dmcdonald21@cnm.edu>
- * @version 4.0.0
+ * Cross Section of a author Profile
+ * @author Jeffrey Gallegos <jgallegos362@cnm.edu>
+ * @version 1.0.0
  **/
 
 class author {
 	use ValidateUuid;
+	/**
+	 * id for this author; this is the primary key
+	 * @var Uuid $authorId
+	 **/
 	private $authorId;
-	private $authorAtHandle;
+	/**
+	 * avatar Url for this author; this is a url for the author's specific avatar
+	 * @var $authorAvatarUrl
+	 */
+	private $authorAvatarUrl;
+	/**
+	 * token handed out to verify that the author is valid and not malicious.
+	 *v@var $authorActivationToken
+	 **/
 	private $authorActivationToken;
+	/**
+	 * email for this author; this is a unique index
+	 * @var string $authorEmail
+	 **/
 	private $authorEmail;
+	/**
+	 * hash for author password
+	 * @var $authorHash
+	 **/
 	private $authorHash;
+	/**
+	 * username for this author; this is a unique index
+	 * @var $authorUsername
+	 */
+	private $authorUsername;
 }
